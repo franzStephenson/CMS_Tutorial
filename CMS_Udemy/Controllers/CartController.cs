49e5a1cf4e1c9ac4f181817e6c032919ddac180f
DeeVexx <DeeVexx@DESKTOP-KM6VH6A>
2018-01-11 06:36:27 -0500

    jan 11 updates

--- CMS_Udemy/Controllers/CartController.cs
+++ CMS_Udemy/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using CMS_Udemy.Models.ViewModels.Cart;
+﻿using CMS_Udemy.Models.Data;
+using CMS_Udemy.Models.ViewModels.Cart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,6 +58,8 @@ namespace CMS_Udemy.Controllers
                     qty += item.Quantity;
                     price += item.Quantity * item.Price;
                 }
+                model.Quantity = qty;
+                model.Price = price;
             }
             else
             {
@@ -67,6 +70,119 @@ namespace CMS_Udemy.Controllers
 
             //Or set
             return PartialView(model);
+        }
+
+        public ActionResult AddToCartPartial(int id)
+        {
+            //Init CartVM list
+            List<CartVM> cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            //Init CartVM
+            CartVM model = new CartVM();
+
+            using(db DB = new db())
+            {
+                //Get the product
+                ProductsDTO product = DB.Products.Find(id);
+
+                //Check if the product is already in cart
+                var productInCart = cart.FirstOrDefault(x => x.ProductId == id);
+
+                //If not , add new
+                if(productInCart == null)
+                {
+                    cart.Add(new CartVM()
+                    {
+                        ProductId = product.ID,
+                        ProdcutName = product.Name, 
+                        Quantity = 1,
+                        Price = product.Price,
+                        Image = product.ImageName
+                    });
+                }
+                else
+                {
+                    //If it is, increment
+                    productInCart.Quantity++;
+                }
+            }
+
+            //Get total qty and price and add to model
+            int qty = 0;
+            decimal price = 0m;
+
+            foreach(var item in cart)
+            {
+                qty += item.Quantity;
+                price += item.Quantity * item.Price;
+            }
+
+            model.Quantity = qty;
+            model.Price = price;
+
+            //Save cart back to seesion
+            Session["cart"] = cart;
+
+
+            //Return partial view with model
+            return PartialView(model);
+        }
+        //GET: /cart/IncrementProduct
+        public JsonResult IncrementProduct(int  productId)
+        {
+            //Init cart list
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using(db DB = new db())
+            {
+                //Get cartVM from list
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                //Increment qty
+                model.Quantity++;
+
+                //store needed data
+                var result = new { qty = model.Quantity, price = model.Price };
+
+
+                //return json with data
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+           
+        }
+
+        //GET: /cart/DecrementProduct
+        public JsonResult DecrementProduct(int productId)
+        {
+            //Init cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using(db DB = new db())
+            {
+                //Get cartVM from list
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                //decrement qty
+                if(model.Quantity > 1)
+                {
+                    model.Quantity--;
+                }
+                else
+                {
+                    model.Quantity = 0;
+                    cart.Remove(model);
+                }
+
+                //store needed data
+                var result = new { qty = model.Quantity, price = model.Price };
+
+                //return json with model
+                return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+
+
+           
         }
     }
 }
